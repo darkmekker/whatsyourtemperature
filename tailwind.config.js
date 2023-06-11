3b02e4b819c57aec901381aff283e4324ae8d35b
Mark Dekker <info@markdekker.com>
2023-06-11 19:04:07 +1200

    Update tailwind.config.js
    Disable css purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -51,7 +51,8 @@ module.exports = {
   plugins: [],
   purge: {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
+    //enabled: process.env.NODE_ENV === 'production',
+    enabled: false,
     content: ['components/**/*.vue', 'layouts/**/*.vue', 'pages/**/*.vue', 'plugins/**/*.js', 'nuxt.config.js'],
     options: {
       // Set safelist in nuxt.config.js -> purgeCSS.safelist: ['btn', 'icon']
